a3a92536bff8559016b553a3b864928118f58d4c
papu1005 <109205367+papu1005@users.noreply.github.com>
2023-04-20 09:39:20 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+﻿#include "mainwindow.h"
+#include <QApplication>
+#include "login.h"
+#pragma execution_character_set("utf-8")
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Login w;
+    w.show();
+
+    return a.exec();
+}
+
